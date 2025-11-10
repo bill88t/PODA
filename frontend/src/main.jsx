94de6fe874679d03f7f2a90ad5ef2bdb7174e0b0
Nick K <cspios@gmail.com>
2025-11-10 12:06:09 +0200

    feat: Added hydrate option for react. Prerenders part to index.html

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,12 @@
-import { createRoot } from 'react-dom/client'
+import { hydrateRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
 
-createRoot(document.getElementById('root')).render(
+hydrateRoot(document,
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<App />} />
         </Routes>
-    </BrowserRouter>,
+    </BrowserRouter>
 )
