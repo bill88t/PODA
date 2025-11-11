23d3f6f0ed80f456943bf445701b8ce977d6c327
Nick K <cspios@gmail.com>
2025-11-11 21:04:49 +0200

    feat: Page not found

---
+++ frontend/src/NotFound.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function NotFound() {
+  return (
+    <>
+      <h1>Page not found</h1>
+    </>
+  )
+}
+
+export default NotFound
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,14 @@
 import { hydrateRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.tsx'
+import NotFound from './NotFound.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
 
 hydrateRoot(document,
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<App />} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     </BrowserRouter>
 )
