314329084b36803894cb99fb695db721497d6e58
Nick K <cspios@gmail.com>
2025-11-16 12:56:48 +0200

    feat: Add auto redirect to page not found to home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,7 +2,12 @@ function App() {
 
   return (
     <>
-        <h1>Start Page</h1>
+        <h1>Welcome to Our Saloon</h1>
+        <p>
+            Our Saloon PADA Saloon is open from 2025
+            our history is new, but we would like to create
+            stories for our community.
+        </p>
     </>
   )
 }
