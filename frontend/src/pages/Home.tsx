14ca1306bcad1a85545e68339236ab8f88a4b17b
Nick K <cspios@gmail.com>
2025-11-21 22:14:55 +0200

    feat: More css. Input with shadow, Brownish text and colored buttons

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -13,6 +13,11 @@ function App() {
                 The PADA Saloon is open from 2025
                 our history is new, but we would like to create
                 stories for our community.
+                We would like to have you here with us
+                and help us leverage your hairstyle,
+                because you deserve that.
+                You are amazing and we are gonna make it
+                appear, so everyone is gonna see it.
             </p>
         </>
     )
