63911d1cdad5b23852814fbaf2ea7448ced7bc09
Nick K <cspios@gmail.com>
2025-11-23 16:53:01 +0200

    feat: Even more css

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -3,10 +3,14 @@ import { Link } from "react-router"
 function NavBar() {
     return (
         <div className="nav">
-            <Link to="/">Home Page</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/sign_up">Sign up</Link>
-            <Link to="/profile">Profile</Link>
+            <div className="nav-left">
+                <Link to="/">Home Page</Link>
+                <Link to="/login">Login</Link>
+                <Link to="/sign_up">Sign up</Link>
+            </div>
+            <div className="nav-right">
+                <Link to="/profile">Profile</Link>
+            </div>
         </div>
     )
 }
