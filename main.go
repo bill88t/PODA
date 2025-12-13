19993bf99b86fb939b5a569598756962bf215cd3
Nick K <cspios@gmail.com>
2025-12-13 23:08:01 +0200

    feat: Add limiter abd tested so that it is not annoying

--- main.go
+++ main.go
@@ -3,18 +3,24 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 )
 
 func main() {
 	// Creates a new Fiber instance.
 	app := fiber.New()
+	app.Use(limiter.New(limiter.Config{
+		Max:        5,
+		Expiration: 1 * time.Second,
+	}))
 
 	// https://github.com/gofiber/recipes/blob/master/react-router/cmd/react-router/main.go
 	// Folder to set up the assets
 	app.Static("/", "./frontend/dist")
-	// Fallvack for React if a file failed
+	// Fallback for React if a file failed
 	app.Static("*", "./frontend/dist/index.html")
 
 	port := os.Getenv("PORT")
