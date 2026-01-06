8060f957e56d64492b46991087aa5f4a319ad2ee
Nick K <cspios@gmail.com>
2026-01-06 13:50:46 +0200

    fix: tested limiter was on by mistake

--- main.go
+++ main.go
@@ -3,19 +3,20 @@ package main
 import (
 	"log"
 	"os"
-	"time"
+
+	// "time"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/limiter"
+	// "github.com/gofiber/fiber/v2/middleware/limiter"
 )
 
 func main() {
 	// Creates a new Fiber instance.
 	app := fiber.New()
-	app.Use(limiter.New(limiter.Config{
-		Max:        5,
-		Expiration: 1 * time.Second,
-	}))
+	// app.Use(limiter.New(limiter.Config{
+	// 	Max:        5,
+	// 	Expiration: 1 * time.Second,
+	// }))
 
 	// https://github.com/gofiber/recipes/blob/master/react-router/cmd/react-router/main.go
 	// Folder to set up the assets
